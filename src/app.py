09756deeca160c21c0d665dcf5a7aac47210f891
Felix Isaac <limfelix16@gmail.com>
2021-06-18 15:36:53 +0800

    feat(src): create garbage

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 import turtle
+import random
 
 screen = turtle.Screen()
 screen.bgcolor('#000000')
@@ -43,4 +44,24 @@ def show_alert(message, width = 200, text_color = 'white', box_color = 'red'):
 		font=('San Serif', 16, 'normal'),
 		align='center'
 	)
+
+show_alert('Welcome to Space Garbage Collector!', 500)
+
+def create_garbage(garbages):
+	(x, y) = turtle.screensize()
+
+	for i in range(garbages):
+		randomX = random.randint(-x, x)
+		randomY = random.randint(-y, y)
+		randomWidth = random.uniform(0.5, 1.5)
+		randomHeight = random.uniform(0.5, 1.5)
+
+		garbage = turtle.Turtle()
+		garbage.speed(0)
+		garbage.hideturtle()
+		garbage.color('red')
+		garbage.shape('circle')
+		garbage.penup()
+		garbage.shapesize(randomWidth, randomHeight)
+		garbage.goto(randomX, randomY)
 
