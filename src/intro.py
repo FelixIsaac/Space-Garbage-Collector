7906a860dde02fc3ba42d6c87125c610f314e35c
Felix Isaac <limfelix16@gmail.com>
2021-12-11 19:07:50 +0800

    update(intro): minor update

--- src/intro.py
+++ src/intro.py
@@ -1,18 +1,20 @@
 from utils.alerts import show_alert
 
 intros = [
-	'Welcome to Space Junk Collector!',
 	'''
+\tWelcome to Space Junk Collector!
+
 There\'s a lot of space junk/garbage orbiting Earth.
-	''',
-	'''
 Collect the junk by shooting bullets.
 Level up when there is no more junk
 
 Move left and right by pressing/holding the
-left or right arrow keys, or 'A' and 'D'
+left or right arrow keys, or 'A' and 'D'.
 
 Shoot by pressing 'W", 'space', or the 'Up' arrow key
+	''',
+	'''
+Space debris/junk is defunct artificial objects in Earth orbit
 	'''
 ]
 
@@ -25,7 +27,7 @@ def start_intro(callback, index = -1):
 		index += 1
 
 		if index < len(intros):
-			show_alert(intros[index], show_intro, 500)
+			show_alert(intros[index], show_intro, 600)
 		else:
 			callback()
 
